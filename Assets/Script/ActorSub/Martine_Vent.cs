d71c089f1f3582bfb86bc2771a25287e39451f26
minjun <mjun1208@naver.com>
2021-09-16 02:00:08 +0900

    마틴 벤트 RPC로 수정

--- Assets/Script/ActorSub/Martine_Vent.cs
+++ Assets/Script/ActorSub/Martine_Vent.cs
@@ -3,37 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Martine_Vent : MonoBehaviour, IPunObservable
+public class Martine_Vent : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Animator _animator;
 
     private bool _isOn = false;
 
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(_isOn);
-        }
-        else
-        {
-            _isOn = (bool)stream.ReceiveNext();
-        }
-
-        if (_isOn)
-        {
-            ActiveVent();
-            _isOn = false;
-            Debug.Log("Ang");
-        }
-    }
-
     public void OnVent()
     {
-        _isOn = true;
+        photonView.RPC("ActiveVent", RpcTarget.All);
     }
 
-    public void ActiveVent()
+    [PunRPC]
+    private void ActiveVent()
     {
         _animator.Rebind();
         _animator.Play("Vent");
