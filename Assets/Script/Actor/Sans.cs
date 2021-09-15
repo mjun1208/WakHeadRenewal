85523d57bc5e1e5bde66e9cff0671ef44276c172
minjun <mjun1208@naver.com>
2021-09-16 02:37:43 +0900

    샌즈 스크립트 추가, 설정

---
+++ Assets/Script/Actor/Sans.cs
@@ -0,0 +1,25 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sans : Actor
+{
+    protected override void Active_Attack()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        photonView.RPC("BoneAttack", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void BoneAttack()
+    {
+        var newbone = Global.PoolingManager.LocalSpawn("Sans_Bone", this.transform.position, Quaternion.identity, true);
+
+        newbone.GetComponent<Sans_Bone>().SetInfo(this.gameObject, GetAttackDir());
+    }
+}
