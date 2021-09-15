f88c80f71ec1cd23095e8ab290b43b5bca0e6362
minjun <mjun1208@naver.com>
2021-09-16 02:36:13 +0900

    샌즈 서브 오브젝트 스크립트 추가

---
+++ Assets/Script/ActorSub/Sans_Bone.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sans_Bone : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private AttackRange _attackRange;
+
+    public const float X_OFFSET = 3f;
+
+    public void SetInfo(GameObject owner, Vector3 dir)
+    {
+        _attackRange.SetOwner(owner);
+
+        this.transform.position = owner.transform.position + new Vector3(X_OFFSET * dir.x, 0, 0);
+
+        _animator.Rebind();
+        _animator.Play("Vent");
+
+        _attackRange.CollidedObjectList.Clear();
+    }
+
+    private void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime > 0.95f)
+        {
+            Global.PoolingManager.LocalDespawn(this.gameObject);
+        }
+    }
+
+    private void OnDamage()
+    {
+        foreach (var targetObject in _attackRange.CollidedObjectList)
+        {
+            targetObject.GetComponent<Entity>().Damaged();
+        }
+    }
+}
