bd1015eea5b35fca56e7d86974892641c1e02913
minjun <mjun1208@naver.com>
2021-09-16 18:52:39 +0900

    물고기 프리팹, 스크립트 추가

---
+++ Assets/Script/ActorSub/AnimalCrossing_Fish.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalCrossing_Fish : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigid;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private List<Sprite> _fishSprites;
+
+    public void SetActive(bool isActive)
+    {
+        this.gameObject.SetActive(isActive);
+
+        if (isActive)
+        {
+            _rigid.AddForce(Vector2.right * 10f, ForceMode2D.Impulse);
+        }
+    }
+
+    public void SelectRandomFish()
+    {
+        var randomIndex = Random.Range(0, _fishSprites.Count - 1);
+
+        _renderer.sprite = _fishSprites[randomIndex];
+    }
+}
