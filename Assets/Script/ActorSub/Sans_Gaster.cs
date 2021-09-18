861b5f9fa9687f7129bc6ff98991dbe026d371a5
minjun <mjun1208@naver.com>
2021-09-18 21:17:14 +0900

    블래스트 피벗 추가, 발사 rpc 추가

--- Assets/Script/ActorSub/Sans_Gaster.cs
+++ Assets/Script/ActorSub/Sans_Gaster.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class Sans_Gaster : MonoBehaviourPunCallbacks
 {
     [SerializeField] private Animator _animator;
+    [SerializeField] private GameObject _blastPivot;
 
     private GameObject owner;
     private Vector3 _originalScale;
     private Vector3 _dir;
 
-    public const float BlastOffset = 1.5f;
+    public const float GasterXOffset = 2f;
+    public const float GasterYOffset = 0.2f;
+    public const float ChargeOffset = 0.2f;
 
     private void Awake()
     {
@@ -20,7 +23,7 @@ public class Sans_Gaster : MonoBehaviourPunCallbacks
 
     public void SetInfo(GameObject owner, Vector3 pos, Vector3 dir)
     {
-        this.transform.position = pos + new Vector3(BlastOffset * dir.x, 0);
+        this.transform.position = pos + new Vector3(GasterXOffset * dir.x, GasterYOffset);
 
         float rotationScale = _originalScale.x * dir.x;
         this.transform.localScale = new Vector3(rotationScale, _originalScale.y, _originalScale.z);
@@ -40,6 +43,19 @@ public class Sans_Gaster : MonoBehaviourPunCallbacks
         _animator.Play("Fire");
     }
 
+
+    public void OnCharging()
+    {
+        photonView.RPC("ActiveCharging", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void ActiveCharging()
+    {
+        var newCharging = Global.PoolingManager.LocalSpawn("Sans_Charge_Blast", _blastPivot.transform.position + _dir * ChargeOffset, Quaternion.identity, true);
+    }
+
+
     public void FireBlast()
     {
         if (!photonView.IsMine)
@@ -55,6 +71,6 @@ public class Sans_Gaster : MonoBehaviourPunCallbacks
     {
         var newBlast = Global.PoolingManager.LocalSpawn("Sans_Gaster_Blast", this.transform.position, Quaternion.identity, true);
 
-        newBlast.GetComponent<Sans_Gaster_Blast>().SetInfo(this.photonView, this.gameObject, _dir);
+        newBlast.GetComponent<Sans_Gaster_Blast>().SetInfo(this.photonView, this.gameObject, _blastPivot.transform.position, _dir);
     }
 }
