9ce8e38c896aaf1258c21c99b2b89d4fc28b7b93
minjun <mjun1208@naver.com>
2021-09-19 21:19:05 +0900

    제트 스킬1, 스킬2, 평타 구현

--- Assets/Script/Actor/Jett.cs
+++ Assets/Script/Actor/Jett.cs
@@ -11,6 +11,22 @@ public class Jett : Actor
     }
 
     private AttackState _attackState = AttackState.Ghost;
+    private int _shurikenCount = 0;
+
+
+    protected override void Update()
+    {
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            _isAttack = true;
+        }
+        else
+        {
+            _isAttack = false;
+        }
+
+        base.Update();
+    }
 
     protected override void Active_Attack()
     {
@@ -34,12 +50,38 @@ public class Jett : Actor
         {
             return;
         }
+
+        _shurikenCount += 10;
+    }
+
+    private void ThrowShuriken()
+    {
+        _shurikenCount--;
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+        if (_shurikenCount == 0)
+        {
+            _animator.SetBool("Is" + _attackState.ToString(), _isAttackInput);
+
+            _animator.SetBool("IsAttack_2_0", false);
+            _animator.SetBool("IsAttack_2_1", false);
+        }
+        else
+        {
+            _animator.SetBool("IsAttack_2_0", _isAttackInput && _shurikenCount % 2 == 0);
+            _animator.SetBool("IsAttack_2_1", _isAttackInput && _shurikenCount % 2 == 1);
+        }
     }
 
     protected override void OnSkill_1()
     {
         if (_isSkill_1Input)
         {
+            _animator.SetBool("Is" + _attackState.ToString(), false);
+
             if (_attackState == AttackState.Ghost)
             {
                 _attackState = AttackState.Operator;
@@ -48,6 +90,8 @@ public class Jett : Actor
             {
                 _attackState = AttackState.Ghost;
             }
+
+            _isDoingSkill = false;
         }
     }
 }
