37ef1d46039654bc312c0c5d4465473f51b5b240
minjun <mjun1208@naver.com>
2021-09-19 19:45:10 +0900

    제트 프리팹 , 스크립트 추가

---
+++ Assets/Script/Actor/Jett.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jett : Actor
+{
+    private enum AttackState
+    {
+        Ghost,
+        Operator,
+    }
+
+    private AttackState _attackState = AttackState.Ghost;
+
+    protected override void Active_Attack()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    protected override void Active_Skill_2()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    protected override void OnSkill_1()
+    {
+        if (_isSkill_1Input)
+        {
+            if (_attackState == AttackState.Ghost)
+            {
+                _attackState = AttackState.Operator;
+            }
+            else
+            {
+                _attackState = AttackState.Ghost;
+            }
+        }
+    }
+}
