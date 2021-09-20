491aaeab92889f0c919d505761328e40b7372981
minjun <mjun1208@naver.com>
2021-09-20 20:02:29 +0900

    나루토 스크립트 추가

---
+++ Assets/Script/Actor/Naruto.cs
@@ -0,0 +1,52 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Naruto : Actor
+{
+    private enum RasenganState
+    {
+        Ready,
+        Charging,
+        Shoot
+    }
+
+    private RasenganState _rasenganState;
+
+    protected override void OnSkill_2()
+    {
+        switch (_rasenganState)
+        {
+            case RasenganState.Ready:
+                {
+                    base.OnSkill_1();
+
+                    if (_isSkill_1)
+                    {
+                    }
+                    break;
+                }
+            case RasenganState.Charging:
+                {
+                    base.OnSkill_1();
+
+                    if (_isSkill_1)
+                    {
+                    }
+                    break;
+                }
+            case RasenganState.Shoot:
+                {
+                    base.OnSkill_1();
+
+                    if (_isSkill_1)
+                    {
+                    }
+                    break;
+                }
+        }
+    }
+
+
+}
