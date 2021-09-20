a5ac57db0651c00e401345b735c93b2c2287707a
minjun <mjun1208@naver.com>
2021-09-21 00:20:37 +0900

    카카시 스크립트 추가

---
+++ Assets/Script/Actor/Kakashi.cs
@@ -0,0 +1,23 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kakashi : Actor
+{
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        var targetList = _skill_1Range.CollidedObjectList;
+
+        foreach (var target in targetList)
+        {
+            var targetEntity = target.GetComponent<Entity>();
+            targetEntity.KnockBack(GetAttackDir(), 1f, 0);
+        }
+    }
+}
