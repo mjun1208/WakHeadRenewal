5a32043d30d3215e87f2004a9aca8158aac986c1
minjun <mjun1208@naver.com>
2021-10-04 04:46:30 +0900

    팬치 스크립트 추가

--- Assets/Script/Chimpanzee/Chimpanzee.cs
+++ Assets/Script/Chimpanzee/Chimpanzee.cs
@@ -4,4 +4,46 @@ using UnityEngine;
 
 public class Chimpanzee : Entity
 {
+    private enum ChimpanzeeState
+    {
+        Move,
+        Attack
+    }
+
+    private ChimpanzeeState _state = ChimpanzeeState.Move;
+    private Entity _targetEntity;
+    // private Entity _targetTower;
+
+    public void SetTarget(Entity entity)
+    {
+        _targetEntity = entity;
+    }
+
+    private void Update()
+    {
+        switch (_state)
+        {
+            case ChimpanzeeState.Move:
+                {
+                    break;
+                }
+            case ChimpanzeeState.Attack:
+                {
+                    break;
+                }
+        }
+    }
+
+    private void Move()
+    {
+        if (_targetEntity == null)
+        {
+
+        }
+    }
+
+    private void Attack()
+    {
+
+    }
 }
