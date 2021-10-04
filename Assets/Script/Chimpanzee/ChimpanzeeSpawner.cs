ce3c1945c513395225e764cab86a8df281733059
minjun <mjun1208@naver.com>
2021-10-05 02:10:46 +0900

    팬치 스포너 추가

---
+++ Assets/Script/Chimpanzee/ChimpanzeeSpawner.cs
@@ -0,0 +1,53 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChimpanzeeSpawner : MonoBehaviourPunCallbacks
+{
+    [SerializeField] private Team _team;
+
+    private void Start()
+    {
+        StartCoroutine(Vow());
+    }
+
+    private IEnumerator Vow()
+    {
+        while (true)
+        {
+            if (PhotonNetwork.IsConnected && PhotonNetwork.IsMasterClient &&
+                !string.IsNullOrWhiteSpace(Global.instance.MyActorName) &&
+                !string.IsNullOrWhiteSpace(Global.instance.EnemyActorName))
+            {
+                Spawn();
+            }
+
+            yield return new WaitForSeconds(5f);
+        }
+    }
+
+    public void Spawn()
+    {
+        string spawnName = "";
+
+        switch (_team)
+        {
+            case Team.BLUE:
+                {
+                    spawnName = Constant.PANZEE_BLUE;
+                    break;
+                }
+            case Team.RED:
+                {
+                    spawnName = Constant.PANZEE_RED;
+                    break;
+                }
+        }
+
+        var randomPos = (Vector3)Random.insideUnitCircle * 3f;
+
+        var newPanzee = PhotonNetwork.Instantiate(spawnName, this.transform.position + randomPos, Quaternion.identity);
+        newPanzee.GetComponent<Entity>().SetTeam(_team);
+    }
+}
