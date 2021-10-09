71b665c316327df6ad2b9979436a191ffd33fd2d
minjun <mjun1208@naver.com>
2021-10-10 05:19:37 +0900

    나루토 스킬 1, 스킬 2 실행 함수 추가

--- Assets/Script/Actor/Naruto.cs
+++ Assets/Script/Actor/Naruto.cs
@@ -14,39 +14,82 @@ public class Naruto : Actor
 
     private RasenganState _rasenganState;
 
+    public override void OnSkill_1()
+    {
+        var newSmoke = Global.PoolingManager.LocalSpawn("Naruto_Smoke", this.transform.position, Quaternion.identity, true);
+        var newDummy = Global.PoolingManager.Spawn("Naruto_Dummy", this.transform.position, Quaternion.identity);
+    }
+
     public override void OnSkill_2()
     {
         switch (_rasenganState)
         {
             case RasenganState.Ready:
                 {
-                    base.OnSkill_1();
+                    base.OnSkill_2();
 
-                    if (isSkill_1)
+                    if (isSkill_2)
                     {
+                        _rasenganState = RasenganState.Charging;
                     }
                     break;
                 }
             case RasenganState.Charging:
                 {
-                    base.OnSkill_1();
-
-                    if (isSkill_1)
+                    if (isSkill_2)
                     {
+                        _rasenganState = RasenganState.Shoot;
                     }
                     break;
                 }
             case RasenganState.Shoot:
                 {
-                    base.OnSkill_1();
+                    isSkill_2 = true;
 
-                    if (isSkill_1)
+                    Debug.Log("vow");
+
+                    if (OnSkillCoroutine == null)
                     {
+                        IsDoingSkill = true;
+                        OnSkillCoroutine = ShootRasengan();
+                        StartCoroutine(OnSkillCoroutine);
+
+                        _animator.SetBool("IsSkill_2", true);
+                        _animator.SetBool("IsCharging", true);
+                    }
+
+                    if (isSkill_2)
+                    {
+                        _rasenganState = RasenganState.Ready;
                     }
                     break;
                 }
         }
     }
 
+    private IEnumerator ShootRasengan()
+    {
+        IsDoingSkill = true;
 
+        while (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Skill_2_2"))
+        {
+            yield return null;
+        }
+
+        yield return null;
+
+        while (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.95f)
+        {
+            yield return null;
+        }
+
+        IsDoingSkill = false;
+        isSkill_1 = false;
+        isSkill_2 = false;
+
+        _animator.SetBool("IsSkill_2", false);
+        _animator.SetBool("IsCharging", false);
+
+        OnSkillCoroutine = null;
+    }
 }
