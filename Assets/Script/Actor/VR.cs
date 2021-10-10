05895da64062c0d42b7bb33c12eb2b30af1db26f
minjun <mjun1208@naver.com>
2021-10-11 01:49:14 +0900

    VR 스크립트 추가

---
+++ Assets/Script/Actor/VR.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR : Actor
+{
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        var targetList = _skill_1Range.CollidedObjectList;
+
+        foreach (var target in targetList)
+        {
+            var targetEntity = target.GetComponent<Entity>();
+            targetEntity.Damaged(target.transform.position);
+        }
+    }
+}
