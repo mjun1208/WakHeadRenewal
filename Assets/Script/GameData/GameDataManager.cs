2ed00c6a3c86f523ad053e1a3bb967d3f7c9ed0b
minjun <mjun1208@naver.com>
2021-10-11 04:09:15 +0900

    게임 데이터 매니저 추가

---
+++ Assets/Script/GameData/GameDataManager.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class GameDataManager
+{
+    public ActorGameDataArray ActorGameData { get; set; }
+
+    private string _assetBundlePath = "Assets/AssetBundles/gamedata";
+
+    public void Load()
+    {
+#if UNITY_EDITOR
+        _assetBundlePath = "Assets/StreamingAssets/gamedata";
+#else
+        _assetBundlePath = Application.dataPath + "/StreamingAssets/gamedata";
+#endif
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(_assetBundlePath));
+        AssetBundle bundle = request.assetBundle;
+
+        TextAsset data = bundle.LoadAsset("ActorData") as TextAsset;
+
+        ActorGameData = JsonUtility.FromJson<ActorGameDataArray>(data.ToString());
+    }
+}
