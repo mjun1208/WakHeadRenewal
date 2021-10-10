ce8289f2fc9a72ae39d643482bef9df7d12ac05f
minjun <mjun1208@naver.com>
2021-10-11 03:19:03 +0900

    픽창용 액터 추가

---
+++ Assets/Script/OutGame/SimpleActor.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleActor : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+
+    [SerializeField] private List<RuntimeAnimatorController> _animatorControllerList;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Confirmed();
+        }
+    }
+
+    public void Select(int index)
+    {
+        _animator.runtimeAnimatorController = _animatorControllerList[index];
+    }
+
+    public void Confirmed()
+    {
+        _animator.Play("Attack");
+    }
+
+    public void Active_Attack()
+    {
+    }
+}
