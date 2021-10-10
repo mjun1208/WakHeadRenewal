9aef7d59bcf3bc256cf173c58307cbba94182142
minjun <mjun1208@naver.com>
2021-10-11 03:19:09 +0900

    픽 매니저 추가

---
+++ Assets/Script/OutGame/PickManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickManager : MonoBehaviour
+{
+    [SerializeField] private SimpleActor _blueActor;
+    [SerializeField] private SimpleActor _redActor;
+
+    public void Blue_Select(int index)
+    {
+        _blueActor.Select(index);
+    }
+
+    public void Red_Select(int index)
+    {
+        _redActor.Select(index);
+    }
+}
