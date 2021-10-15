72199811e34290d4761e15625248f25039882e1f
minjun <mjun1208@naver.com>
2021-10-16 03:14:35 +0900

    용암 스크립트 추가

--- Assets/Script/ActorSub/Minecraft_Lava.cs
+++ Assets/Script/ActorSub/Minecraft_Lava.cs
@@ -1,8 +1,35 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Minecraft_Lava : ActorSub 
+public class Minecraft_Lava : ActorSub
 {
-    
+    [SerializeField] private Dictionary<int, GameObject> _lavaDic = new Dictionary<int, GameObject>();
+
+    public void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 pos, Vector3 dir)
+    {
+        base.SetInfo(ownerPhotonView, owner, dir);
+
+        this.transform.position = pos;
+    }
+
+    public void ActiveDamage()
+    {
+        if (_attackRange.CollidedObjectList.Count > 0)
+        {
+            foreach (var targetObject in _attackRange.CollidedObjectList)
+            {
+                OnDamage(targetObject.GetComponent<Entity>());
+            }
+        }
+    }
+
+    protected override void OnDamage(Entity entity)
+    {
+        if (_ownerPhotonView.IsMine)
+        {
+            entity.Damaged(this.transform.position);
+        }
+    }
 }
