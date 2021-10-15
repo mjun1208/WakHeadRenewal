5d09fc7ace90de0a289d610ee92efc84f503ce35
김민준 <mjun1208@meerkatgames.co.kr>
2021-10-15 15:44:04 +0900

    나루토 더미 추가

---
+++ Assets/Script/ActorSub/Naruto_Dummy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class Naruto_Dummy : ActorSub
+{
+    [SerializeField] private Animator _animator;
+
+    public void Attack(bool isAttack)
+    {
+        _animator.SetBool("IsAttack", isAttack);
+    }
+    
+    public void SetAnimationParameter(string name, bool isTrue)
+    {
+        _animator.SetBool(name, isTrue);
+    }
+
+    public void ShootRasengan()
+    {
+        
+    }
+}
