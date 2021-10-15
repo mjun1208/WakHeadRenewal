7da025d41c66762d93079911bd032f5923bb2189
minjun <mjun1208@naver.com>
2021-10-16 04:19:14 +0900

    마크 스크립트 추가, 스킬 2 액티브 추가

---
+++ Assets/Script/Actor/Minecraft.cs
@@ -0,0 +1,33 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minecraft : Actor
+{
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    protected override void Active_Skill_2()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        photonView.RPC("LavaElemant", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void LavaElemant()
+    {
+        var newLava = Global.PoolingManager.LocalSpawn("Minecraft_Lava", this.transform.position, Quaternion.identity, true);
+
+        newLava.GetComponent<Minecraft_Lava>().SetInfo(this.photonView, this.gameObject, this.transform.position, GetAttackDir());
+    }
+}
