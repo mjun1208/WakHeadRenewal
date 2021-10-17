b968b406c99b42263e891d62b94f329b53359ca0
minjun <mjun1208@naver.com>
2021-10-18 03:10:15 +0900

    VR 스킬2 (투명화 추가)

--- Assets/Script/Actor/VR.cs
+++ Assets/Script/Actor/VR.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class VR : Actor
 {
+    protected override void ForceStop(bool isStun)
+    {
+        base.ForceStop(isStun);
+
+        if (isStun)
+        {
+            photonView.RPC("DisInvisibilityRPC", RpcTarget.All);
+        }
+    }
+
     protected override void Active_Skill_1()
     {
         if (!photonView.IsMine)
@@ -18,5 +30,50 @@ public class VR : Actor
             var targetEntity = target.GetComponent<Entity>();
             targetEntity.Damaged(target.transform.position);
         }
+    }
+
+    protected override void Attack()
+    {
+        base.Attack();
+
+        if (_isAttackInput)
+        {
+            photonView.RPC("DisInvisibilityRPC", RpcTarget.All);
+        }
+    }
+
+    public override void OnSkill_1()
+    {
+        base.OnSkill_1();
+
+        photonView.RPC("DisInvisibilityRPC", RpcTarget.All);
+    }
+
+    public override void OnSkill_2()
+    {
+        isSkill_2 = true;
+        IsDoingSkill = true;
+
+        photonView.RPC("InvisibilityRPC", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void InvisibilityRPC()
+    {
+        _animator.Rebind();
+
+        float targetAlpha = photonView.IsMine ? 0.5f : 0f;
+
+        _renderer.DOColor(new Color(1, 1, 1, targetAlpha), 0.8f).SetEase(Ease.InOutBack).OnComplete(() =>
+        {
+            isSkill_2 = false;
+            IsDoingSkill = false;
+        });
+    }
+
+    [PunRPC]
+    public void DisInvisibilityRPC()
+    {
+        _renderer.color = new Color(1, 1, 1, 1);
     }
 }
