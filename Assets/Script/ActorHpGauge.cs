a51e8e4a882f9e151850bf54184e9731f1f365eb
minjun <mjun1208@naver.com>
2021-10-31 05:39:56 +0900

    HP 게이지 스크립트 추가

---
+++ Assets/Script/ActorHpGauge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActorHpGauge : MonoBehaviour
+{
+    [SerializeField] private Image _hpGaugeImage;
+
+    private void Update()
+    {
+        if (Global.instance.MyActor != null) {
+
+            if (Global.instance.MyActor.HP > 0)
+            {
+                _hpGaugeImage.fillAmount = ((float)Global.instance.MyActor.HP / (float)Global.instance.MyActor.MaxHP);
+            }
+            else
+            {
+                _hpGaugeImage.fillAmount = 0f;
+            }
+        }
+    }
+}
