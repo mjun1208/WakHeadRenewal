8cf2dec5f6e07e78d924f53fa3b879eb4022db59
minjun <mjun1208@naver.com>
2021-11-01 03:20:22 +0900

    픽 정보 옮기는거 적용

--- Assets/Script/OutGame/PickSync.cs
+++ Assets/Script/OutGame/PickSync.cs
@@ -5,4 +5,23 @@ using Photon.Pun;
 
 public class PickSync : MonoBehaviourPunCallbacks, IPunObservable
 {
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(PickManager.Instance.CurrentMyActorID);
+            stream.SendNext(PickManager.Instance.IsMyReady);
+        }
+        else
+        {
+            PickManager.Instance.EnemyActorSelect((int)stream.ReceiveNext());
+        
+            bool isReady = (bool)stream.ReceiveNext();
+            if (isReady != PickManager.Instance.IsEnemyReady)
+            {
+                PickManager.Instance.EnemyReady();
+            }
+            PickManager.Instance.IsEnemyReady = isReady;
+        }
+    }
 }
