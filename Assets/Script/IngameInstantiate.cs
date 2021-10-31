d8c600525701a1a5dc3dafde50f8cb24c58e3ce9
minjun <mjun1208@naver.com>
2021-11-01 02:04:35 +0900

    인게임 들어올 시 ActorSpawner 생성

---
+++ Assets/Script/IngameInstantiate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IngameInstantiate : MonoBehaviour
+{
+    private void Start()
+    {
+        Photon.Pun.PhotonNetwork.Instantiate("ActorSpawner", Vector3.zero, Quaternion.identity);
+    }
+}
