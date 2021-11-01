785cc32ae2867f6e3f79048c525ebd4a021dab58
minjun <mjun1208@naver.com>
2021-11-02 01:34:15 +0900

    픽창 SimpleActor Update 제거

--- Assets/Script/OutGame/SimpleActor.cs
+++ Assets/Script/OutGame/SimpleActor.cs
@@ -8,14 +8,6 @@ public class SimpleActor : MonoBehaviour
 
     [SerializeField] private List<RuntimeAnimatorController> _animatorControllerList;
 
-    public void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.X))
-        {
-            Confirmed();
-        }
-    }
-
     public void Select(int index)
     {
         _animator.runtimeAnimatorController = _animatorControllerList[index];
