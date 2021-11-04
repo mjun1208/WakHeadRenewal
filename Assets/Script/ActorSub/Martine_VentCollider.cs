17630570fedb6867398791234df4d5162aed9f8a
minjun <mjun1208@naver.com>
2021-11-05 02:53:29 +0900

    마틴 벤트 전용 콜라이더 추가

---
+++ Assets/Script/ActorSub/Martine_VentCollider.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Martine_VentCollider : MonoBehaviour
+{
+    [SerializeField] private Martine _martine;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _martine.VentColliderEnter(collision);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _martine.VentColliderExit(collision);
+    }
+}
