312e1c451b7de6054b67cc523b11bb2336bdb7f2
minjun <mjun1208@naver.com>
2021-11-06 16:12:48 +0900

    더미 안죽게 수정

--- Assets/Script/Actor/Dummy.cs
+++ Assets/Script/Actor/Dummy.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class Dummy : Entity
 {
+    protected override void Awake()
+    {
+        base.Awake();
+
+        MaxHP = 99999;
+        HP = MaxHP;
+    }
+
+    private void Update()
+    {
+        HP = 99999;
+    }
 }
