e242b3b8f97e08d8b8e9a94f6f66e4f57813fac2
minjun <mjun1208@naver.com>
2021-11-07 20:16:10 +0900

    프로필 아이콘 스크립트 추가

---
+++ Assets/Script/UI/ProfileIcon.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProfileIcon : MonoBehaviour
+{
+    [SerializeField] private Team _team;
+    [SerializeField] private Image _profileIconImage;
+
+    private Actor _targetActor;
+    private string _actorName;
+
+    private void Start()
+    {
+        switch (_team)
+        {
+            case Team.BLUE:
+                {
+                    Global.instance.BlueActorSetAction += SetActor;
+                    break;
+                }
+            case Team.RED:
+                {
+                    Global.instance.RedActorSetAction += SetActor;
+                    break;
+                }
+        }
+    }
+
+    private void SetActor(Actor actor)
+    {
+        _targetActor = actor;
+
+        if (Global.instance.MyTeam == _team)
+        {
+            _actorName = Global.instance.MyActorName;
+        }
+        else
+        {
+            _actorName = Global.instance.EnemyActorName;
+        }
+
+        var profileIconName = $"{_actorName}_Profile_Icon";
+
+        _profileIconImage.sprite = Global.GameDataManager.FindProfileIcon(profileIconName);
+    }
+}
