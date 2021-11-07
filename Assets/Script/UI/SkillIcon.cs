50d01e6497176e1896a7992e9ee855eceae30202
minjun <mjun1208@naver.com>
2021-11-07 20:05:33 +0900

    스킬아이콘 설정 로직 추가

--- Assets/Script/UI/SkillIcon.cs
+++ Assets/Script/UI/SkillIcon.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillIcon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Team _team;
+    [SerializeField] private Image _skillIconImage;
+    [SerializeField] private int _skillNum;
+
+    private Actor _targetActor;
+    private string _actorName;
+
+    private void Start()
     {
-        
+        switch (_team)
+        {
+            case Team.BLUE:
+                {
+                    Global.instance.BlueActorSetAction += SetActor;
+                    break;
+                }
+            case Team.RED:
+                {
+                    Global.instance.RedActorSetAction += SetActor;
+                    break;
+                }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SetActor(Actor actor)
     {
-        
+        _targetActor = actor;
+
+        if (Global.instance.MyTeam == _team)
+        {
+            _actorName = Global.instance.MyActorName;
+        }
+        else
+        {
+            _actorName = Global.instance.EnemyActorName;
+        }
+
+        var skillIconName = $"{_actorName}_Skill_{_skillNum}_Icon";
+
+        _skillIconImage.sprite = Global.GameDataManager.FindSkillIcon(skillIconName);
     }
 }
