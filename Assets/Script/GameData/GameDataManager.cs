92e2b2c47fb92a4409e3a165139b9f6d84856640
minjun <mjun1208@naver.com>
2021-11-07 20:04:22 +0900

    스킬 아이콘 리소스 찾기 추가

--- Assets/Script/GameData/GameDataManager.cs
+++ Assets/Script/GameData/GameDataManager.cs
@@ -8,20 +8,59 @@ public class GameDataManager
 {
     public ActorGameDataArray ActorGameData { get; set; }
 
-    private string _assetBundlePath = "Assets/AssetBundles/gamedata";
+    private Object[] _loadedSkillIcon;
 
     public void Load()
     {
+        GameDataLoad();
+        SkillIconLoad();
+    }
+
+    public void GameDataLoad()
+    {
+        string assetBundlePath;
+
 #if UNITY_EDITOR
-        _assetBundlePath = "Assets/StreamingAssets/gamedata";
+        assetBundlePath = "Assets/StreamingAssets/gamedata";
 #else
-        _assetBundlePath = Application.dataPath + "/StreamingAssets/gamedata";
+        assetBundlePath = Application.dataPath + "/StreamingAssets/gamedata";
 #endif
-        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(_assetBundlePath));
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(assetBundlePath));
         AssetBundle bundle = request.assetBundle;
 
         TextAsset data = bundle.LoadAsset("ActorData") as TextAsset;
 
         ActorGameData = JsonUtility.FromJson<ActorGameDataArray>(data.ToString());
+    }
+
+    public void SkillIconLoad()
+    {
+        string assetBundlePath;
+
+#if UNITY_EDITOR
+        assetBundlePath = "Assets/StreamingAssets/skillicon";
+#else
+        assetBundlePath = Application.dataPath + "/StreamingAssets/skillicon";
+#endif
+        AssetBundleCreateRequest request = AssetBundle.LoadFromMemoryAsync(File.ReadAllBytes(assetBundlePath));
+        AssetBundle bundle = request.assetBundle;
+
+        _loadedSkillIcon = bundle.LoadAllAssets();
+    }
+    public Sprite FindSkillIcon(string name)
+    {
+        foreach (var skillIcon in _loadedSkillIcon)
+        {
+            if (skillIcon.name == name)
+            {
+                var skillIconTex = skillIcon as Texture2D;
+
+                Sprite skillIconSprite = Sprite.Create(skillIconTex, new Rect(0.0f, 0.0f, skillIconTex.width, skillIconTex.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+                return skillIconSprite as Sprite;
+            }
+        }
+
+        return null;
     }
 }
