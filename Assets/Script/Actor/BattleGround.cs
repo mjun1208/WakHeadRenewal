377e1786916065af001906c06aa4ea82820a1f51
minjun <mjun1208@naver.com>
2021-11-07 23:48:39 +0900

    배그 스크립트 추가

---
+++ Assets/Script/Actor/BattleGround.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGround : Actor
+{
+    protected override void Active_Attack()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        _attackRange.Attack(targetEntity =>
+        {
+            targetEntity.KnockBack(4, GetAttackDir(), 0.5f, 0);
+        });
+    }
+
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+
+    protected override void Active_Skill_2()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+    }
+}
