2633dc7cd266851b645eb3f9a8a3045ce62b74c7
minjun <mjun1208@naver.com>
2021-11-13 02:47:25 +0900

    투척물 폭발 스크립트 추가

---
+++ Assets/Script/ActorSub/BattleGround_Throw_Bomb.cs
@@ -0,0 +1,31 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGround_Throw_Bomb : ActorSub
+{
+    private BattleGround.ThrowType _throwType;
+
+    public void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 pos, Vector3 dir, BattleGround.ThrowType throwType)
+    {
+        base.SetInfo(ownerPhotonView, owner, dir);
+
+        _throwType = throwType;
+
+        this.transform.position = pos;
+    }
+
+    private void OnDamage()
+    {
+        if (_ownerPhotonView == null || !_ownerPhotonView.IsMine)
+        {
+            return;
+        }
+
+        _attackRange.Attack(targetEntity =>
+        {
+            targetEntity.Damaged(this.transform.position, 5);
+        });
+    }
+}
