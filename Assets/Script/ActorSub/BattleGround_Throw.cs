64404a5bca6628e1156496d0694d13ed74b42716
minjun <mjun1208@naver.com>
2021-11-13 02:47:31 +0900

    투척물 스크립트 추가

---
+++ Assets/Script/ActorSub/BattleGround_Throw.cs
@@ -0,0 +1,68 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGround_Throw : ActorSub
+{
+    private BattleGround.ThrowType _throwType;
+
+    public void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 dir, BattleGround.ThrowType throwType)
+    {
+        base.SetInfo(ownerPhotonView, owner, dir);
+
+        _throwType = throwType;
+
+        _moveSpeed = Constant.BATTLEGROUND_THROW_MOVE_SPEED;
+
+        StartCoroutine(Go());
+    }
+
+    private void Update()
+    {
+        this.transform.localEulerAngles += new Vector3(0, 0, 30) * Time.deltaTime;
+
+        _attackRange.AttackEntity(targetEntity =>
+        {
+            OnDamage(targetEntity, 5);
+        }, true);
+        _attackRange.AttackSummoned(targetSummoned =>
+        {
+            if (_ownerPhotonView.IsMine)
+            {
+                targetSummoned.Damaged(targetSummoned.transform.position);
+            }
+            OnDamage(null, 5);
+        }, true);
+    }
+
+    protected override void OnDamage(Entity entity, int damage)
+    {
+        StopAllCoroutines();
+
+        if (_ownerPhotonView.IsMine)
+        {
+            entity?.KnockBack(damage, _dir, 1f, 0f);
+        }
+
+        Destroy();
+    }
+
+    protected override void OnDestory(ActorSub actorSub)
+    {
+        base.OnDestory(actorSub);
+
+        if (_ownerPhotonView == null || !_ownerPhotonView.IsMine)
+        {
+            return;
+        }
+
+        var newBomb = Global.PoolingManager.LocalSpawn($"BattleGround_Throw_{(BattleGround.ThrowType)_throwType}_Bomb", this.transform.position, Quaternion.identity, false);
+        newBomb.GetComponent<BattleGround_Throw_Bomb>().SetInfo(_ownerPhotonView, _owner, this.transform.position, _dir, _throwType);
+    }
+
+    public override void Destroy()
+    {
+        DestoryAction?.Invoke(this);
+    }
+}
