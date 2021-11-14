aa21c25263e94e4a3be4c0b14555535bf987727f
minjun <mjun1208@naver.com>
2021-11-15 02:38:40 +0900

    배그 에임 기능 구현

--- Assets/Script/ActorSub/BattleGround_Aim.cs
+++ Assets/Script/ActorSub/BattleGround_Aim.cs
@@ -6,63 +6,149 @@ using UnityEngine;
 public class BattleGround_Aim : ActorSub
 {
     [SerializeField] private GameObject _aimObject;
+    [SerializeField] private SpriteRenderer _backGround;
     [SerializeField] private Animator _animator;
 
-    private const float AimSpeed = 10f;
+    public Vector3 AimPosition
+    {
+        get
+        {
+            return _aimObject.transform.position;
+        }
+        set
+        {
+            _aimObject.transform.position = value;
+        }
+    }
 
-    private int _shootCount = 5;
+    public int ShootCount { get; private set; } = 5;
+
+    private bool _isShooting = false;
+    private IEnumerator _onShootingCoroutine = null;
 
     public override void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 dir)
     {
         base.SetInfo(ownerPhotonView, owner, dir);
 
-        _aimObject.transform.position = Vector3.zero;
+        _onShootingCoroutine = null;
+        _isShooting = false;
+        _aimObject.transform.localPosition = Vector3.zero;
 
-        _shootCount = 5;
+        ShootCount = 5;
+
+        if (!_ownerPhotonView.IsMine)
+        {
+            _backGround.color = new Color(0, 0, 0, 0);
+        }
+        else
+        {
+            _backGround.color = new Color(1, 1, 1, 130f / 255f);
+        }
     }
 
     private void Update()
     {
+        if (!_ownerPhotonView.IsMine || _isShooting)
+        {
+            return;
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            _aimObject.transform.position += Vector3.right * AimSpeed * Time.deltaTime;
+            AimPosition += Vector3.right * Constant.BATTLEGROUND_AIM_MOVE_SPEED * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            _aimObject.transform.position += Vector3.left * AimSpeed * Time.deltaTime;
+            AimPosition += Vector3.left * Constant.BATTLEGROUND_AIM_MOVE_SPEED * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            _aimObject.transform.position += Vector3.up * AimSpeed * Time.deltaTime;
+            AimPosition += Vector3.up * Constant.BATTLEGROUND_AIM_MOVE_SPEED * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            _aimObject.transform.position += Vector3.down * AimSpeed * Time.deltaTime;
-        }
-
-        if (Input.GetKey(KeyCode.C))
-        {
-            Shoot();
+            AimPosition += Vector3.down * Constant.BATTLEGROUND_AIM_MOVE_SPEED * Time.deltaTime;
         }
     }
 
-    public void Shoot()
+    protected IEnumerator OnShooting()
     {
+        _isShooting = true;
+
+        while (!_animator.GetCurrentAnimatorStateInfo(0).IsName("Aim_Shoot"))
+        {
+            yield return null;
+        }
+
+        yield return null;
+
+        while (_animator.GetCurrentAnimatorStateInfo(0).normalizedTime < 0.9f)
+        {
+            yield return null;
+        }
+
+        // end
+
+        _isShooting = false;
+
+        _onShootingCoroutine = null;
+
+        if (ShootCount <= 0)
+        {
+            Destroy();
+        }
+    }
+
+    public void Shoot(Vector3 shootPosition)
+    { 
+        if (!this.gameObject.activeSelf)
+        {
+            return;
+        }
+
+        AimPosition = shootPosition;
+
+        _onShootingCoroutine = OnShooting();
+        StartCoroutine(_onShootingCoroutine);
+
         _animator.Play("Aim_Shoot");
     }
 
     public void ReduceShootCount()
     {
-        if (!_ownerPhotonView.IsMine)
-        {
-            return;
-        }
+        ShootCount--;
 
-        _shootCount--;
-
-        if (_shootCount <= 0)
+        _attackRange.AttackEntity(targetEntity =>
         {
-            Destroy();
+            OnDamage(targetEntity, 10);
+        }, true);
+        _attackRange.AttackSummoned(targetSummoned =>
+        {
+            if (_ownerPhotonView.IsMine)
+            {
+                targetSummoned.Damaged(targetSummoned.transform.position);
+            }
+            OnDamage(null, 10);
+        }, true);
+
+        Global.PoolingManager.LocalSpawn("SnipeHitEffect", _aimObject.transform.position, Quaternion.identity, true);
+    }
+
+    protected override void OnDamage(Entity entity, int damage)
+    {
+        if (_ownerPhotonView.IsMine)
+        {
+            entity?.Damaged(this.transform.position, damage);
         }
+    }
+
+    public override void Destroy()
+    {
+        StopAllCoroutines();
+        _onShootingCoroutine = null;
+
+        DestoryAction?.Invoke(this);
+
+        Global.PoolingManager.Despawn(this.gameObject);
     }
 }
