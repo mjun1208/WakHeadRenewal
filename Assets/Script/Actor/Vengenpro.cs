2bdf52e9297a2f300b0d93a163cccb4ca6fd8b15
minjun <mjun1208@naver.com>
2021-11-18 01:53:04 +0900

    벤젠프로 스크립트 추가

---
+++ Assets/Script/Actor/Vengenpro.cs
@@ -0,0 +1,51 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vengenpro : Actor
+{
+    protected override void Active_Attack()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        _attackRange.AttackRandom(targetEntity => 
+        {
+            targetEntity.KnockBack(4, GetAttackDir(), 0.5f, 0);
+        });
+    } 
+
+    protected override void Active_Skill_1()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        _skill_1Range.Attack(targetEntity =>
+        {
+            targetEntity.Grab(10, this.transform.position, 10f);
+        });
+    }
+
+    protected override void Active_Skill_2()
+    {
+        if (!photonView.IsMine)
+        {
+            return;
+        }
+
+        photonView.RPC("ShootBullet", RpcTarget.All);
+    }
+
+    [PunRPC]
+    public void ShootBullet()
+    {
+        var newBullet = Global.PoolingManager.LocalSpawn("Normal_Bullet", this.transform.position, Quaternion.identity, true);
+
+        newBullet.GetComponent<Normal_Bullet>().SetInfo(this.photonView, this.gameObject, GetAttackDir());
+    }
+}
