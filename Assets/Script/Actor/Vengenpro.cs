3262fdaf833ac06907c969a11456c7c49bebb823
minjun <mjun1208@naver.com>
2021-11-19 01:58:08 +0900

    음표 날라가는 로직 추가

--- Assets/Script/Actor/Vengenpro.cs
+++ Assets/Script/Actor/Vengenpro.cs
@@ -5,6 +5,43 @@ using UnityEngine;
 
 public class Vengenpro : Actor
 {
+    float _attackPressTime = 0f;
+    float _attackPressFullCharingTime = 0f;
+    float _attackPressDelay = 0f;
+
+    protected override void Update()
+    {
+        base.Update();
+
+        if (_isAttack && _attackPressDelay <= 0f)
+        {
+            _attackPressDelay = 0f;
+
+            if (_attackPressTime < 5f)
+            {
+                _attackPressTime += Time.deltaTime;
+                _animator.SetFloat("AttackSpeed", 1 + _attackPressTime * 0.5f);
+            }
+            else
+            {
+                _attackPressFullCharingTime += Time.deltaTime;
+            }
+
+            if (_attackPressFullCharingTime >= 1.5f)
+            {
+                _attackPressFullCharingTime = 0f;
+                _attackPressDelay = 5f;
+            }
+        }
+        else
+        {
+            _attackPressDelay -= Time.deltaTime;
+
+            _attackPressTime = 0;
+            _animator.SetFloat("AttackSpeed", 1);
+        }
+    }
+
     protected override void Active_Attack()
     {
         if (!photonView.IsMine)
@@ -12,11 +49,13 @@ public class Vengenpro : Actor
             return;
         }
 
-        _attackRange.AttackRandom(targetEntity => 
+        for (int i = 0; i < 3; i++)
         {
-            targetEntity.KnockBack(4, GetAttackDir(), 0.5f, 0);
-        });
-    } 
+            Vector2 randomDir = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+
+            photonView.RPC("ShootNote", RpcTarget.All, randomDir);
+        }
+    }
 
     protected override void Active_Skill_1()
     {
@@ -47,5 +86,13 @@ public class Vengenpro : Actor
         var newBullet = Global.PoolingManager.LocalSpawn("Normal_Bullet", this.transform.position, Quaternion.identity, true);
 
         newBullet.GetComponent<Normal_Bullet>().SetInfo(this.photonView, this.gameObject, GetAttackDir());
+    }
+
+    [PunRPC]
+    public void ShootNote(Vector2 randomDir)
+    {
+        var newNote = Global.PoolingManager.LocalSpawn("Vengenpro_Note", this.transform.position, Quaternion.identity, true);
+
+        newNote.GetComponent<Vengenpro_Note>().SetInfo(this.photonView, this.gameObject, randomDir);
     }
 }
