20277853b9f40d07023c8722ba762c26f79e743b
minjun <mjun1208@naver.com>
2021-11-21 05:16:10 +0900

    온라인 플레이어 UI 추가

---
+++ Assets/Script/OutGame/UIOnlinePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOnlinePlayer : MonoBehaviour
+{
+    [SerializeField] private Text _lobbyPlayerCountText;
+
+    private void Start()
+    {
+        UpdatePlayerCount();
+    }
+
+    private void Update()
+    {
+        UpdatePlayerCount();
+    }
+
+    private void UpdatePlayerCount()
+    {
+        _lobbyPlayerCountText.text = $"현재 접속자 수 : {PhotonNetwork.CountOfPlayers} 명";
+    }
+}
