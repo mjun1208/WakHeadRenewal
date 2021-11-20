45da272d2179b9478df17e6877dc392939a93adb
minjun <mjun1208@naver.com>
2021-11-21 05:15:42 +0900

    플레이어 찾는 중 UI 스크립트 추가

---
+++ Assets/Script/OutGame/UIFindPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIFindPlayer : MonoBehaviour
+{
+    [SerializeField] private Text _findPlayerText;
+    
+    private void OnEnable()
+    {
+        TextAnimation();
+    }
+
+    public void TextAnimation()
+    {
+        _findPlayerText.DOText("플레이어 찾는 중.....", 1f).From("플레이어 찾는 중", true, false)
+            .OnComplete(TextAnimation);
+    }
+}
