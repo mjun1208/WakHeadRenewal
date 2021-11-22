6dfdc4637819f0c8fc79ae2be200f3f34aff7c3d
minjun <mjun1208@naver.com>
2021-11-22 22:48:29 +0900

    배경 오브젝트 방향 랜덤성 추가

---
+++ Assets/Script/BackgroundObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.localScale = Random.Range(0, 2) == 0 ? new Vector3(1, 1, 1) : new Vector3(-1, 1, 1);
+    }
+}
