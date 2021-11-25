8285119b93c0dd31d3ce5b92a6fa6bc6df880009
김민준 <mjun1208@meerkatgames.co.kr>
2021-11-25 19:46:39 +0900

    죽을 때 카메라 연출 추가

--- Assets/Script/CameraManager.cs
+++ Assets/Script/CameraManager.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace WakHead
@@ -7,9 +9,11 @@ namespace WakHead
     public class CameraManager : MonoBehaviour
     {
         public static CameraManager instance;
-
         public Transform TargetTransform { get; set; }
 
+        [SerializeField] private Camera _myCamera;
+        private bool _isDeadAction = false;
+        
         void Awake()
         {
             instance = this;
@@ -18,7 +22,7 @@ namespace WakHead
         // Update is called once per frame
         void Update()
         {
-            if (TargetTransform != null)
+            if (TargetTransform != null && !_isDeadAction)
             {
                 MoveToTarget();
             }
@@ -41,6 +45,37 @@ namespace WakHead
             }
 
             this.transform.position = Vector3.Lerp(this.transform.position, targetPos, 10f * Time.deltaTime);
+        }
+
+        public void Dead(Action deadAction, Vector3 targetPosition)
+        {
+            _isDeadAction = true;
+            Vector3 originalPos = this.transform.position;
+            
+            Vector3 targetPos = targetPosition;
+            targetPos.z = this.transform.position.z;
+
+            Time.timeScale = 0.2f;
+            this.transform.position = targetPos;
+            _myCamera.DOFieldOfView(15, 0.5f);
+            transform.DOMoveX(targetPos.x, 0.5f);
+            transform.DOMoveY(targetPos.y, 0.5f);
+            _myCamera.DOShakePosition(0.4f, 0.1f).OnComplete(() =>
+            {
+                deadAction?.Invoke();
+                
+                _myCamera.DOFieldOfView(60, 0.5f);
+                this.transform.position = originalPos;
+                Time.timeScale = 1f;
+
+                _isDeadAction = false;
+                
+                _myCamera.DOShakePosition(3f, 0.5f);
+                _myCamera.DOShakeRotation(3f, 0.5f).OnComplete(() =>
+                {
+                    this.transform.rotation = Quaternion.identity;
+                });
+            });
         }
     }
 }
