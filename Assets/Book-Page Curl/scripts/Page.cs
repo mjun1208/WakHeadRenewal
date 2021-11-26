b8cf98580b54303b7074679b5aadb628c8d02a13
minjun <mjun1208@naver.com>
2021-11-27 03:55:31 +0900

    책을 위한 페이지 추가

---
+++ Assets/Book-Page Curl/scripts/Page.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Page : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _actorInfo = new List<GameObject>();
+
+    public int Index
+    {
+        get
+        {
+            return _index;
+        }
+        set
+        {
+            SetIndex(value);
+        }
+    }
+
+    private int _index { get; set; } = -1;
+
+    private void SetIndex(int index)
+    {
+        _index = index;
+
+        foreach (var actor in _actorInfo)
+        {
+            actor.SetActive(false);
+        }
+
+        if (_index != -1)
+        {
+            _actorInfo[_index].SetActive(true);
+        }
+    }
+}
