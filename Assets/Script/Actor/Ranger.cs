dc550a671e75e90c12aa28de6f7f69012bc432f5
김민준 <mjun1208@meerkatgames.co.kr>
2021-12-03 19:28:25 +0900

    느그레인저 스크립트 추가

---
+++ Assets/Script/Actor/Ranger.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WakHead
+{
+    public class Ranger : Actor
+    {
+        [SerializeField] private GameObject _attackBeam;
+        
+        protected override void Active_Attack()
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+
+            _attackRange.Attack(targetEntity => { targetEntity.Damaged( targetEntity.transform.position, 3, MyTeam, 
+                "TreeSkill_2Effect"); }, MyTeam);
+        }
+        
+        protected override void Attack()
+        {
+            base.Attack();
+            
+            if (!_isAttackInput)
+            {
+                _animator.SetBool("IsAttackLoop", _isAttackInput);
+            }
+            
+            _attackBeam.SetActive(_isAttackInput);
+        }
+
+        protected override void CheckAttack()
+        {
+            if (_animator.GetCurrentAnimatorStateInfo(0).IsName("Attack") || _animator.GetCurrentAnimatorStateInfo(0).IsName("AttackLoop"))
+            {
+                _isAttack = true;
+            }
+            else
+            {
+                _isAttack = false;
+            }
+        }
+
+        public void AttackLoop()
+        {
+            _animator.SetBool("IsAttackLoop", true);
+        }
+        
+        protected override void Dead()
+        {
+            base.Dead();
+            
+            _attackBeam.SetActive(false);
+        }
+    }
+}
