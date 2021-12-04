69bf17f80fa12149d608cf7f7f1b1eaa8fc3bb91
minjun <mjun1208@naver.com>
2021-12-05 01:46:36 +0900

    카멘타이탄 추가

---
+++ Assets/Script/ActorSub/Ranger_Titan.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+using WakHead;
+
+namespace WakHead
+{
+    public class Ranger_Titan : ActorSub
+    {
+        private Vector3 _originalScale;
+
+        private readonly Vector3 offset = new Vector3(5.5f, 3f);
+
+        private void Awake()
+        {
+            _originalScale = this.transform.localScale;
+        }
+        
+        public override void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 dir, Team team = Team.None)
+        {
+            base.SetInfo(ownerPhotonView, owner, dir, team);
+
+            this.transform.position += new Vector3(offset.x * dir.x, offset.y); 
+            
+            float rotationScale = _originalScale.x * -dir.x;
+            this.transform.localScale = new Vector3(rotationScale, _originalScale.y, _originalScale.z);
+        }
+
+        public void ActiveDamage()
+        {
+            _attackRange.Attack(targetEntity => { OnDamage(targetEntity, 30); }, MyTeam);
+        }
+        
+        protected override void OnDamage(Entity entity, int damage)
+        {
+            if (_ownerPhotonView.IsMine)
+            {
+                entity?.Damaged(this.transform.position, damage, MyTeam, 
+                    "TreeSkill_2Effect");
+            }
+        }
+    }
+}
