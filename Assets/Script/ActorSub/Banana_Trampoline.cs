3d060f66c4f43aca2f05ca94986ae8d3c48c57d6
minjun <mjun1208@naver.com>
2021-12-05 05:23:09 +0900

    트램펄린 추가

---
+++ Assets/Script/ActorSub/Banana_Trampoline.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+namespace WakHead
+{
+    public class Banana_Trampoline : Summoned
+    {
+        public void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 pos, Vector3 dir, Team team = Team.None)
+        {
+            base.SetInfo(ownerPhotonView, owner, dir, team);
+
+            transform.position = pos;
+            
+            MaxHP = 5;
+            HP = MaxHP;
+        }
+    }
+}
