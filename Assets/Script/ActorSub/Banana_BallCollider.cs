da142e3ca1559e14ad8dc38322098a44ce581f19
minjun <mjun1208@naver.com>
2021-12-06 02:53:39 +0900

    공 콜라이더 추가

---
+++ Assets/Script/ActorSub/Banana_BallCollider.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WakHead
+{
+    public class Banana_BallCollider : MonoBehaviour
+    {
+        [SerializeField] private Banana _banana;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            _banana.BallColliderEnter(collision);
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            _banana.BallColliderExit(collision);
+        }
+    }
+}
