3207030a8112bdee205ef47f73e4d1ccaa5e3385
minjun <mjun1208@naver.com>
2021-12-09 02:25:47 +0900

    랜덤 컬러 쉐이더 추가

---
+++ Assets/AllIn1SpriteShader/Demo/Scripts/RandomColorShader.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class RandomColorShader : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Material mat;
+    
+    private int mainColorPropertyShaderID;
+    private int glowColorPropertyShaderID;
+
+    private void Start()
+    {
+        mainColorPropertyShaderID = Shader.PropertyToID("_Color");
+        glowColorPropertyShaderID = Shader.PropertyToID("_GlowColor");
+    }
+
+    private void OnEnable()
+    {
+        Color changeColor = GenerateColor();
+
+        _renderer.color = changeColor;
+        mat.SetColor(mainColorPropertyShaderID, changeColor);
+        mat.SetColor(glowColorPropertyShaderID, changeColor);
+    }
+
+    private Color GenerateColor()
+    {
+        return new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f), 1f);
+    }
+}
