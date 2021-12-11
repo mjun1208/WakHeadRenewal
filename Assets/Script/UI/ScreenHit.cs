9335d77b2c71ef07d1b030d1a66c83dd8c97e514
minjun <mjun1208@naver.com>
2021-12-11 18:52:49 +0900

    맞았을 때 화면 효과 추가

---
+++ Assets/Script/UI/ScreenHit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.UI;
+using WakHead;
+
+public class ScreenHit : MonoBehaviour
+{
+    [SerializeField] private Image _screenHitImage;
+
+    public void OnEnable()
+    {
+        _screenHitImage.DOColor(new Color(1, 1, 1, 0f), 0.5f).OnComplete(() =>
+        {
+            _screenHitImage.color = new Color(1, 1, 1, 1f);
+            Global.PoolingManager.LocalDespawn(this.gameObject);
+        });
+    }
+}
