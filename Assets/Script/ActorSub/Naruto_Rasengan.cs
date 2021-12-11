ee53120dfd4e20c0fdb8fe4596592506ba537a65
minjun <mjun1208@naver.com>
2021-12-11 16:55:17 +0900

    나루토 나선환 사이즈에 따라 딜 버프

--- Assets/Script/ActorSub/Naruto_Rasengan.cs
+++ Assets/Script/ActorSub/Naruto_Rasengan.cs
@@ -8,6 +8,7 @@ namespace WakHead
     public class Naruto_Rasengan : ActorSub
     {
         private Vector3 _originalScale;
+        private int _size;
 
         public void SetInfo(PhotonView ownerPhotonView, GameObject owner, Vector3 pos, Vector3 dir, int size, Team team = Team.None)
         {
@@ -18,6 +19,8 @@ namespace WakHead
             _moveSpeed = Constant.NARUTO_RASENGAN_MOVE_SPEED;
 
             _originalScale = new Vector3(2 + size, 2 + size, 2 + size);
+
+            _size = size;
 
             float rotationScale = _originalScale.x * dir.x;
             this.transform.localScale = new Vector3(rotationScale, _originalScale.y, _originalScale.z);
@@ -38,7 +41,7 @@ namespace WakHead
 
         public void ActiveDamage()
         {
-            _attackRange.Attack(targetEntity => { OnDamage(targetEntity, 1); }, MyTeam);
+            _attackRange.Attack(targetEntity => { OnDamage(targetEntity, _size * 3); }, MyTeam);
         }
 
         protected override void OnDamage(Entity entity, int damage)
